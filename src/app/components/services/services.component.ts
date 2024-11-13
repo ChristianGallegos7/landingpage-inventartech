d50b144f4479f87caeb0cd5787797c30cd512561
CHRISTIAN G. GALLEGOS YAGUACHI <cgallegos@inventar-tech.ad>
2024-11-13 13:29:40 -0500

    Nueva card para servicios

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // Importa el módulo de Font Awesome
+import { faLaptop, faLightbulb, faRocket } from '@fortawesome/free-solid-svg-icons'; // Importa los íconos
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
-
+  faLaptop = faLaptop; // Asigna los íconos a variables
+  faLightbulb = faLightbulb;
+  faRocketlaunch = faRocket;
 }
