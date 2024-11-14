6b76faa916d2a1d7afc40fbdcbb7085196bd3313
CHRISTIAN G. GALLEGOS YAGUACHI <cgallegos@inventar-tech.ad>
2024-11-14 10:09:57 -0500

    Servicios con aos ready

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // Importa el módulo de Font Awesome
 import { faLaptop, faLightbulb, faRocket, faLaptopCode } from '@fortawesome/free-solid-svg-icons'; // Importa los íconos
+import Aos from 'aos';
 
 @Component({
   selector: 'app-services',
@@ -9,9 +10,18 @@ import { faLaptop, faLightbulb, faRocket, faLaptopCode } from '@fortawesome/free
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
   faLaptop = faLaptop; // Asigna los íconos a variables
   faLightbulb = faLightbulb;
   faRocketlaunch = faRocket;
   faLaptopCode = faLaptopCode;
+
+
+
+  ngOnInit(): void {
+    Aos.init({
+      duration: 1200,
+      once: true
+    });
+  }
 }
