47e639d22780e47a474f3c6aa8904644247fa489
CHRISTIAN G. GALLEGOS YAGUACHI <cgallegos@inventar-tech.ad>
2024-11-21 11:02:42 -0500

    Nuevo proveedor

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import Aos from 'aos';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
